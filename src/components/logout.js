beb2d843a27a2b22bf210837046538789cc28677
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-20 17:37:46 -0400

    logout funct.todo: connect to app

---
+++ src/components/logout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const Logout = () => {
+    const history = useHistory();
+    localStorage.removeItem('token')
+    localStorage.removeItem('role')
+    history.push('/classlist')
+    return(
+        <></>
+    )
+}
+
+export default Logout;
