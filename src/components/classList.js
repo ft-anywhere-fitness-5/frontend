06df92c845b625eaab82871d49ffeacf6b055037
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-20 01:50:55 -0400

    flex bug and text tweak to classList and Class

--- src/components/classList.js
+++ src/components/classList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getClasses } from "../actions";
 import Class from './class';
+import styled from "styled-components";
 
 const ClassList = (props) => {
 
@@ -11,14 +12,14 @@ const ClassList = (props) => {
 
 
     return(
-      <div>
+      <StyledClass>
         <h3>Classes</h3>
         <div className='classlist'>
           {props.classes && props.classes.map(classItem => {
             return <Class key={classItem.class_id} classItem={classItem}/>
           })}
         </div>
-      </div>
+      </StyledClass>
     )
 }
 
@@ -27,3 +28,36 @@ const mapStateToProps= state => ({
 })
 
 export default connect(mapStateToProps,{getClasses})(ClassList) 
+
+const StyledClass = styled.div`
+  border: 1px solid gold;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .classlist{
+    background: black;
+    width:35%;
+  }
+  h2 {
+    display:flex;
+    font-size: 200%;
+    color: gold;
+    justify-content:center;
+    margin-bottom: 4rem;
+  }
+  p {
+    display: flex;
+    color: black;
+    font-size: 1.5rem;
+    font-weight: bold;
+    background: gold;
+    justify-content: center;
+    margin-left: 5rem;
+    width: 75%;
+    height: 2.3rem;
+  }
+  p:hover {
+    background: white;
+    transform: scale(1.04);
+  }
+`
